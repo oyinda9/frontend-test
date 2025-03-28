93c1e634331c430bd3333829f62cb490a76a8efb
sholanke precious <sholankeprecious3@gmail.com>
2025-03-28 12:46:07 +0100

    removed config in next.config

--- pdf-upload-test/next.config.ts
+++ pdf-upload-test/next.config.ts
@@ -2,13 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    workerThreads: true,
-    optimizePackageImports: [
-      '@react-pdf-viewer/core',
-      'pdf-lib'
-    ]
-  }
+  
 };
 
 export default nextConfig;
