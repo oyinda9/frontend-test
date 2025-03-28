d27be721da40ff7182f75efb28a656453fa8e6f4
sholanke precious <sholankeprecious3@gmail.com>
2025-03-28 12:38:33 +0100

    added a configuration to optimise build

--- pdf-upload-test/next.config.ts
+++ pdf-upload-test/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    workerThreads: true,
+    optimizePackageImports: [
+      '@react-pdf-viewer/core',
+      'pdf-lib'
+    ]
+  }
 };
 
 export default nextConfig;
